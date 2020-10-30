42e8bea26582685cfb5f16d42b4e49cd10dfb7ae
Kris Fernandez <kfernz2626@gmail.com>
2020-10-30 22:22:29 +0800

    Users and User Types Migration

--- database/migrations/2020_10_30_140650_create_users_table.php
+++ database/migrations/2020_10_30_140650_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('userTypeId')->unsigned();
             $table->foreign('userTypeId')->references('id')->on('users_type');
             $table->string('firstName');
             $table->string('lastName');
