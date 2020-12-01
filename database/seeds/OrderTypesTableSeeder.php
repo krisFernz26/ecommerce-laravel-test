7028e0950cadf1a06a38efc55273f5af046a26d4
John <johnmichaelfajardo499@gmail.com>
2020-12-02 00:18:28 +0800

    Database Seeder for Orders

---
+++ database/seeds/OrderTypesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OrderTypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        $order_types = [
+
+        	['name' => 'Phone', 'description' => Phone]
+        	['name' => 'Book', 'description' => Book]
+
+
+        ]
+    }
+}
