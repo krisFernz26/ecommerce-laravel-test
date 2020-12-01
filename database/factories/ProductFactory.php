c05882cd1247cbbd717be5fd4d7e36e0002697ab
JarmRafols <rayjellejarm@gmail.com>
2020-12-01 11:13:55 +0800

    database_seeders_for_products_and_product_types

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+
+    	'product_type_id' => mt_rand(1,5),
+    	'post_id' => mt_rand(1,20),
+    	'name' => $faker->name,
+    	'quantity' => mt_rand(0, 20),
+    	'image' => $faker->imageUrl(); 
+    ];
+});
